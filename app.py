e826ec962d0933458fa8d02b00f0fc3d95a70a63
Nandan <nandan15012004@gmail.com>
2025-06-16 14:12:39 +0530

    Initial commit for Render deployment

--- app.py
+++ app.py
@@ -111,4 +111,4 @@ def home():
     return render_template('index.html', prediction=prediction, probability=probability)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
